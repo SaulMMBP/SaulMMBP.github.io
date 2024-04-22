5498a27d769aff97007b63bf7ca55d5073ad55c2
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2024-04-21 18:39:30 -0600

    change the button style

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,5 +31,7 @@ let openNav = () => {
 let onSubmit = (event) => {
     event.preventDefault();
 
-    let form = document.getElementById("contact-form");
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
 };
